a17ad72e40b56e4529d2e87a516912e63b0b603e
ghermaico135 <ghermaico135@gmail.com>
2024-10-17 06:40:31 -0700

    Adding the animation using useref and useEffect

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,33 @@
+import { useRef, useEffect } from "react";
+import { init } from 'ityped';
 import "./intro.css"
 
+
 export default function Intro() {
+
+  const textRef = useRef();
+  const isInitialized = useRef(false); 
+
+  useEffect(() => {
+    if (!isInitialized.current && textRef.current) {
+      init(textRef.current, {
+        showCursor: true,
+        typeSpeed: 100,
+        backDelay: 1500,
+        backSpeed: 100,
+        strings: ["Developer", "Designer", "Content Creator", "Digital Marketer"]
+      });
+      isInitialized.current = true; 
+    }
+    
+    return () => {
+      if (textRef.current) {
+        textRef.current.textContent = ''; // Cleanup text on unmount
+      }
+    };
+  }, []);
+
+  
   return (
     <div className="intro" id="intro">
       <div className="left-intro">
@@ -12,7 +39,7 @@ export default function Intro() {
         <div className="wrapper-intro">
           <h2>Hi There I'am</h2>
           <h1>Michael okbamichael</h1>
-          <h3>Freelance <span></span></h3>
+          <h3>Freelance <span className="intro-span" ref={textRef}></span></h3>
           <div>
               <a className="intro-link"  href="#portfolio">
               <img className="downIcon" src="assets/down.png" alt="downIcon" />
