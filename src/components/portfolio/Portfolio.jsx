b3a3d8a1f203852c5cc80ec73afc8bb41c6ce4ca
ghermaico135 <ghermaico135@gmail.com>
2024-10-21 08:12:43 -0700

    Adding profile

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,6 +2,54 @@ import "./portfolio.css"
 
 export default function Portfolio() {
   return (
-    <div className="portfolio" id="portfolio"></div>
+    <div className="portfolio" id="portfolio">
+      <h1 className="portfolio-heading">Portfolio</h1>
+      <ul className="portfolio-menu">
+          <li className="active">Featured</li>
+          <li>Web App</li>
+          <li>Mobile App</li>
+          <li>Design</li>
+          <li>Branding</li>
+      </ul>
+      <div className="portfolio-container">
+        <div className="items">
+          <img src="assets/mobile.png" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="items">
+          <img src="assets/mobile.png" alt="" />
+          <h3>Banking App</h3>
+        </div>
+
+        <div className="items">
+          <img src="assets/mobile.png" alt="" />
+          <h3>Banking App</h3>
+        </div>
+
+        <div className="items">
+          <img src="assets/mobile.png" alt="" />
+          <h3>Banking App</h3>
+        </div>
+
+        <div className="items">
+          <img src="assets/mobile.png" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        
+        <div className="items">
+          <img src="assets/mobile.png" alt="" />
+          <h3>Banking App</h3>
+        </div>
+
+        <div className="items">
+          <img src="assets/mobile.png" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="items">
+          <img src="assets/mobile.png" alt="" />
+          <h3>Banking App</h3>
+        </div>
+      </div>
+</div>
   )
 }
