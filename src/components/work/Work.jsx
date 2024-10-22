f1a38db30ed2b348f07488c0254bb4022d936b7a
ghermaico135 <ghermaico135@gmail.com>
2024-10-22 03:12:26 -0700

    Adding work section

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -2,6 +2,15 @@ import "./work.css"
 
 export default function Work() {
   return (
-    <div className="works" id="works"></div>
+    <div className="works" id="works">
+      <div className="slider">
+        <div className="container">
+          <div className="item">
+            <div className="left">left</div>
+            <div className="right">right</div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
